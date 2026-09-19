150bc5a2d738e60037fbbb614cbeab6e83e55771
jusanghyeok <jsmh991102@gmail.com>
2026-09-19 12:34:54 +0900

    feat: add local2 and external2 navigation

--- react03-lifecycle/src/Components/TopNavi.jsx
+++ react03-lifecycle/src/Components/TopNavi.jsx
@@ -6,6 +6,8 @@ const TopNavi = () => {
       <NavLink to = "/">수명 주기</NavLink>&nbsp;&nbsp;
       <NavLink to = "/local">내부 통신</NavLink>&nbsp;&nbsp;
       <NavLink to = "/external">외부 통신</NavLink>&nbsp;&nbsp;
+      <NavLink to = "/local2">내부통신2</NavLink>&nbsp;&nbsp;
+      <NavLink to = "/external2">외부통신2</NavLink>&nbsp;&nbsp;
     </nav>
   )
 }

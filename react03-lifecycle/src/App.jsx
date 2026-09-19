ad9fab6bb9abeb866fd17dd4a09584e90b764de9
jusanghyeok <jsmh991102@gmail.com>
2026-09-19 12:34:49 +0900

    feat: add local2 and external2 routes

--- react03-lifecycle/src/App.jsx
+++ react03-lifecycle/src/App.jsx
@@ -3,19 +3,22 @@ import TopNavi from './Components/TopNavi';
 import Lifecycle from './Components/Lifecycle';
 import LocalJsonFetcher from './Components/LocalJsonFetcher';
 import ExternalApiFetcher from './Components/ExternalApiFetcher';
+import LocalJsonFetcher2 from './Components/LocalJsonFetcher2';
+import ExternalApiFetcher2 from './Components/ExternalApiFetcher2';
+
 function App() {
-
-
   return (
     <>
-    <TopNavi></TopNavi>
-    <Routes>
-      <Route path = "/" element = {<Lifecycle />} />
-      <Route path = "/local" element = {<LocalJsonFetcher />} />
-      <Route path = "/external" element = {<ExternalApiFetcher />} />
-    </Routes>
+      <TopNavi></TopNavi>
+      <Routes>
+        <Route path="/" element={<Lifecycle />} />
+        <Route path="/local" element={<LocalJsonFetcher />} />
+        <Route path="/external" element={<ExternalApiFetcher />} />
+        <Route path="/local2" element={<LocalJsonFetcher2 />} />
+        <Route path="/external2" element={<ExternalApiFetcher2 />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
